05faf383d5dadd555aadf226f2f2ea0d3e47779c
Louiel176 <louielian176@gmail.com>
2023-04-19 21:15:16 +0000

    Set up structure of game function

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer() {
+
+}
+function incrementScore(){
+
+}
+function incrementWrongScore(){
+
+}
+function displayAdditionalQuestion(){
+
+}
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+
+}
+
+function displayDivisionQuestion(){
+
+}
